7da475f2fd7e9e2c959c216a39ff4a38339101b9
cntfk2017 <cntfk2017@gmail.com>
2021-05-17 16:04:53 +0800

    Start
    起始方案
    開始學習

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# line_message_collector
+# LINE BOT 收集資料
  收集LINE資料
+
+## 教學文
+* [從LINE BOT到資料視覺化：賴田捕手 系列](https://ithelp.ithome.com.tw/users/20120178/ironman/2654)
+* [只要有心，人人都可以做卡米狗 系列](https://ithelp.ithome.com.tw/users/20107309/ironman/1253)
+* [Line Bot 教學](https://github.com/yaoandy107/line-bot-tutorial#line-bot-教學)
+
+## 資料出處
+* [Line Bot 教學](https://github.com/yaoandy107/line-bot-tutorial#line-bot-教學)
+* [HeroKu CLI Software](https://devcenter.heroku.com/articles/heroku-cli#download-and-install)
---
+++ app-core.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)
+
+# Channel Access Token
+line_bot_api = LineBotApi('hjHb8raZSh34EoQHWvHBrUtPU1JfXX7gVu6Y9oMeJ76Oklf3gilc7oFGGl4+0hoe54Gx4Ea4nV2Vw7fwdkEY3W1JZyzguJbr3kRjAZsvBA9ruwtEfBSlTJoukiHtVYJNaWsIQ+8ri7SIrZxlE4CAbwdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('694dae22097e54525ff160df6662581c')
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
